93c4d3e51afeadef72f11baf7bb9f626b4ba777b
kennedyifeadi <kennedyifeadi@gmail.com>
2024-10-18 22:43:47 +0100

    Refactor Nav component styles and add settings icon

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,18 +2,26 @@ import { IoBookOutline } from "react-icons/io5";
 import { LuBook } from "react-icons/lu";
 import { CiBookmark } from "react-icons/ci";
 import { SiGoogletranslate } from "react-icons/si";
+import { CiSettings } from "react-icons/ci";
 
 export const Nav = () =>{
     return(
-        <div className="w-[18%] h-full shadow-[0px_2px_5px_#00000065]">
-            <nav className="flex flex-col items-center pt-4 bg-[#FFFFFF] w-full h-full ">
+        <div className="w-[18%] h-full shadow-[0px_0px_5px_#00000031]">
+            <nav className="flex flex-col items-center justify-between pt-4 bg-[#FFFFFF] w-full h-full ">
                 <div className="text-[#6200EA] font-bold text-[36px]">Lexi<span className="text-[#FF5722]">Find</span></div>
-                <div className="w-full h-[50%]  mt-8 ">
-                    <ul className="flex flex-col w-full h-full justify-around pl-4 ">
-                        <li className="text-[#333333] text-[24px]"><a href="" className="flex md:items-center md:gap-1"><LuBook /> Find word</a></li>
-                        <li className="text-[#333333] text-[24px]"><a href="" className="flex md:items-center md:gap-1"><SiGoogletranslate /> Translate</a></li>
-                        <li className="text-[#333333] text-[24px]"><a href="" className="flex md:items-center md:gap-1"><IoBookOutline /> Thesaurus</a></li>
-                        <li className="text-[#333333] text-[24px]"><a href="" className="flex md:items-center md:gap-1"><CiBookmark /> Bookmarks</a></li>
+                <div className="w-full h-[40%] mb-40 ">
+                    <ul className="flex flex-col w-full h-full justify-around px-4 ">
+                        <li className="text-[#333333] text-[16px]"><a href="" className="flex p-2 bg-[#E0E0E0] rounded-r-full md:items-center md:gap-1"><LuBook className="text-[18px]" /> Find word</a></li>
+                        <li className="text-[#333333] text-[16px]"><a href="" className="flex p-2 md:items-center md:gap-1"><SiGoogletranslate className="text-[18px]" /> Translate</a></li>
+                        <li className="text-[#333333] text-[16px]"><a href="" className="flex p-2 md:items-center md:gap-1"><IoBookOutline className="text-[18px]" /> Thesaurus</a></li>
+                        <li className="text-[#333333] text-[16px]"><a href="" className="flex p-2 md:items-center md:gap-1"><CiBookmark className="text-[18px]" /> Bookmarks</a></li>
+                    </ul>
+                </div>
+                <div className="w-full h-[10%]">
+                    <ul className="flex flex-col w-full h-full justify-center pl-7 pr-4 ">
+                        <li className="text-[#333333] text-[15px]">
+                            <a href="" className="flex p-2 md:items-center md:gap-1"><CiSettings className="text-[24px]" /> Settings</a>
+                        </li>
                     </ul>
                 </div>
             </nav>
---
+++ src/components/Dictionary.jsx
@@ -0,0 +1,9 @@
+
+
+export const dictionary = () =>{
+    return(
+        <div>
+            
+        </div>
+    )
+}
