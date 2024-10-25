fe8aaae11e0bb5bb71ddeb03efd920c3ecea3674
kennedyifeadi <kennedyifeadi@gmail.com>
2024-10-25 11:54:05 +0100

    Refactor Dailyword and Dicsearch components for UI improvements

--- src/components/Dicsearch.jsx
+++ src/components/Dicsearch.jsx
@@ -23,21 +23,54 @@ export const SearchWord = () => {
   });
 
   return (
-    <div className='flex flex-col w-full max-h-[50%] h-max z-20'>
+    <div className="flex flex-col w-full max-h-[50%] h-max z-20">
       <div className="w-full px-4 h-max flex gap-4">
-
         <input
           type="text"
           name="searchWord"
           placeholder={placeholderTexts[currentPlaceholder]}
           className={`w-[80%] h-[50px] border-2 outline-none border-[#6200EA] px-4 italic rounded-full `}
         />
-        
-        <span className="rounded-full flex items-center justify-center text-white cursor-pointer hover:bg-[#ff5622e0] duration-500 bg-[#FF5722] h-[50px] text-[20px] w-[12%]">Search</span>
+
+        <span className="rounded-full flex items-center  justify-center text-white cursor-pointer hover:shadow-[0px_0px_5px_#0000008a] duration-500 bg-[#FF5722] h-[50px] text-[20px] w-[12%]">
+          Search
+        </span>
       </div>
-      <div className='w-full max-h-[300px] h-[300px] border border-black px-4'>
-        <div></div>
-        <div></div>
+      <div className="w-full max-h-[300px] h-[300px] flex ">
+        <div className="flex-1 flex flex-col h-[300px] p-4 before:w-[2px] before:h-[80%] relative before:absolute before:top-[5%] before:right-0 before:bg-[#e7d6de] ">
+          <div className='flex w-full justify-between items-center'>
+            <h1 className='font-bold text-[18px] flex justify-center items-center gap-2'> Word
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-5 text-[#6200EA] cursor-pointer"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z"
+              />
+            </svg>
+            </h1>
+            <span className='text-[16px] border-2 border-[#6200EA] px-2 py-1 italic rounded-full'>#/ euile /#</span>
+          </div>
+          <p>
+            <span></span>
+          </p>
+        </div>
+        <div className="flex-1 h-[300px] p-4">
+          <div className="flex justify-between w-full">
+            <h1 className='text-[18px]'>Use Case</h1>
+            <span className='text-[16px] border-2 border-[#6200EA] py-1 px-2 rounded-full'>Verb </span>
+          </div>
+          <div className="flex flex-col w-full h-max">
+            <p></p>
+            <p></p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -12,7 +12,7 @@ export const Greetings = () =>{
 
     return(
         <div className="w-full h-[25%] px-6 py-4 z-20">
-            <h1 className="font-bold text-[48px] relative w-[30%]  "><span className="text-[#6200EA]">Hey</span> there <iframe src="https://lottie.host/embed/4947a3e2-a083-4c17-82be-a2b0c1022af3/0sT4zRxHoK.json" className="w-20 h-20 rotate-[-50deg] absolute top-[-10px] right-2"></iframe> </h1>
+            <h1 className="font-bold text-[48px] relative w-[30%]  "><span className="text-[#6200EA]">Hey</span> there <iframe src="https://lottie.host/embed/e111ef25-f77d-4548-85f5-4f608f7400e1/10fJnrj0sg.lottie" className="w-20 h-20 rotate-[-50deg] absolute top-[-10px] right-2"></iframe></h1>
             <h1 className="font-bold text-[48px]">Good <span className="text-[#FF5722]">{greeting}!</span></h1>
         </div>
     )
