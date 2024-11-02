e48e5474b48db2538465195103a9f0fa90497a6e
kennedyifeadi <kennedyifeadi@gmail.com>
2024-11-03 00:35:05 +0100

    Refactor package dependencies and add @lottiefiles/react-lottie-player

--- src/components/Dicsearch.jsx
+++ src/components/Dicsearch.jsx
@@ -1,10 +1,12 @@
-import { useState, useEffect, useCallback, useRef } from 'react';
-import { useNavigate, useSearchParams } from 'react-router-dom';
-import axios from 'axios';
+import { useState, useEffect, useCallback, useRef } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { Player } from "@lottiefiles/react-lottie-player";
+import axios from "axios";
+import { div } from "framer-motion/client";
 
 export const SearchWord = () => {
   const [searchParams] = useSearchParams();
-  const searchWord = searchParams.get('q') || '';
+  const searchWord = searchParams.get("q") || "";
   const [word, setWord] = useState("");
   const navigate = useNavigate();
   const [definitions, setDefinitions] = useState([]);
@@ -12,56 +14,67 @@ export const SearchWord = () => {
   const [examples, setExamples] = useState([]);
   const [audio, setAudio] = useState([]);
   const [wordExist, setWordExist] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const audioRef = useRef(null); // Reference for the audio element
 
   const handleChange = (e) => {
-    setWord(e.target.value)
+    setWord(e.target.value);
     navigate(`/?q=${e.target.value}`, { replace: true });
-  }
-
+  };
+  const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
   const fetchDictionary = async (query) => {
     try {
-      const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`);
-
-      const wordDefinitions = response.data.flatMap(entry =>
-        entry.meanings.flatMap(meaning =>
-          meaning.definitions.map(def => ({
+      const response = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`
+      );
+      await sleep(1000);
+      setIsLoading(false);
+      const wordDefinitions = response.data.flatMap((entry) =>
+        entry.meanings.flatMap((meaning) =>
+          meaning.definitions.map((def) => ({
             definition: def.definition,
-            partOfSpeech: meaning.partOfSpeech
+            partOfSpeech: meaning.partOfSpeech,
           }))
         )
       );
 
-      const wordExamples = response.data.flatMap(entry =>
-        entry.meanings.flatMap(meaning =>
+      const wordExamples = response.data.flatMap((entry) =>
+        entry.meanings.flatMap((meaning) =>
           meaning.definitions
-            .filter(def => def.example) 
-            .map(def => ({
+            .filter((def) => def.example)
+            .map((def) => ({
               example: def.example,
-              partOfSpeech: meaning.partOfSpeech
+              partOfSpeech: meaning.partOfSpeech,
             }))
         )
       );
 
-      const wordTranscriptions = response.data.flatMap(e => (e.phonetics.map(p => p.text)));
-      const wordAudio = response.data.flatMap(e => (e.phonetics.map(p => p.audio)));
+      const wordTranscriptions = response.data.flatMap((e) =>
+        e.phonetics.map((p) => p.text)
+      );
+      const wordAudio = response.data.flatMap((e) =>
+        e.phonetics.map((p) => p.audio)
+      );
 
       setExamples(wordExamples.filter(Boolean));
       setAudio(wordAudio.filter(Boolean));
       setTranscriptions(wordTranscriptions.filter(Boolean));
-      setDefinitions(wordDefinitions.filter(Boolean))
+      setDefinitions(wordDefinitions.filter(Boolean));
     } catch (error) {
       setWordExist(false);
     }
   };
 
-  const groupedDefinitions = definitions.reduce((acc, { definition, partOfSpeech }) => {
-    if (!acc[partOfSpeech]) {
-      acc[partOfSpeech] = [];
-    }
-    acc[partOfSpeech].push(definition);
-    return acc;
-  }, {});
+  const groupedDefinitions = definitions.reduce(
+    (acc, { definition, partOfSpeech }) => {
+      if (!acc[partOfSpeech]) {
+        acc[partOfSpeech] = [];
+      }
+      acc[partOfSpeech].push(definition);
+      return acc;
+    },
+    {}
+  );
 
   const groupedExamples = examples.reduce((acc, { example, partOfSpeech }) => {
     if (!acc[partOfSpeech]) {
@@ -71,22 +84,32 @@ export const SearchWord = () => {
     return acc;
   }, {});
 
-  const handleSearch = useCallback((e) => {
-    e.preventDefault();
-    if (word) {
-      navigate(`/?q=${word}`, { replace: true });
-      fetchDictionary(word);
-      setWordExist(true)
-    }
-  }, [word, navigate]);
+  const handleSearch = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (word) {
+        navigate(`/?q=${word}`, { replace: true });
+        fetchDictionary(word);
+        setWordExist(true);
+        setIsLoading(true);
+      }
+    },
+    [word, navigate]
+  );
 
-  const placeholderTexts = ["Search Word...", "Search Phrase...", "Search Idiom..."];
+  const placeholderTexts = [
+    "Search Word...",
+    "Search Phrase...",
+    "Search Idiom...",
+  ];
   const [currentPlaceholder, setCurrentPlaceholder] = useState(0);
 
   useEffect(() => {
     const changePlaceholder = () => {
       setTimeout(() => {
-        setCurrentPlaceholder((prevIndex) => (prevIndex + 1) % placeholderTexts.length);
+        setCurrentPlaceholder(
+          (prevIndex) => (prevIndex + 1) % placeholderTexts.length
+        );
       }, 500);
     };
 
@@ -100,11 +123,10 @@ export const SearchWord = () => {
     }
   };
 
-
   return (
     <div className="flex flex-col w-full max-h-[50%] h-max z-20">
       <div className="w-full px-4 h-max flex gap-4">
-        <form onSubmit={handleSearch} className='w-full h-max flex gap-4'>
+        <form onSubmit={handleSearch} className="w-full h-max flex gap-4">
           <input
             type="text"
             name="searchWord"
@@ -113,20 +135,20 @@ export const SearchWord = () => {
             placeholder={placeholderTexts[currentPlaceholder]}
             className={`w-[90%] h-[50px] border-2 outline-none border-[#6200EA] px-4 italic rounded-full `}
           />
-          <button className="rounded-full flex items-center  justify-center text-white cursor-pointer hover:shadow-[0px_0px_5px_#0000008a] duration-500 bg-[#FF5722] h-[50px] text-[20px] w-[12%]">
+          <button className="rounded-full ease-in-out flex items-center outline-none  justify-center text-white cursor-pointer hover:shadow-[0px_0px_5px_#0000008a] duration-500 bg-[#FF5722] h-[50px] text-[20px] w-[12%]">
             Search
           </button>
         </form>
       </div>
       <div className="w-full max-h-[300px] h-[300px] flex ">
         <div className="flex-1 flex flex-col h-[300px] p-4 before:w-[2px] before:h-[80%] relative before:absolute before:top-[5%] before:right-0 before:bg-[#e7d6de] ">
-          <div className='flex w-full justify-between items-center'>
-            <h1 className='font-bold text-[18px] flex justify-center items-center gap-2 capitalize'>
-              {wordExist ?
-              word || 'Word'
-              :
-              <span className='text-[red]'>{word}</span>
-              }
+          <div className="flex w-full justify-between items-center">
+            <h1 className="font-bold text-[18px] flex justify-center items-center gap-2 capitalize">
+              {wordExist ? (
+                word || "Word"
+              ) : (
+                <span className="text-[red]">{word}</span>
+              )}
               <svg
                 onClick={playAudio}
                 xmlns="http://www.w3.org/2000/svg"
@@ -144,16 +166,96 @@ export const SearchWord = () => {
               </svg>
               <audio ref={audioRef} src={audio[0]} className="hidden" />
             </h1>
-            <span className='text-[16px] border-2 border-[#6200EA] px-2 py-1 italic rounded-full'>
-              {wordExist ?
-              transcriptions && transcriptions.length > 0 ? transcriptions[0] : '/##/'
+            <span className="text-[16px] border-2 border-[#6200EA] px-2 py-1 italic rounded-full">
+              {
+              isLoading 
+              ? 
+              wordExist 
+              ?
+              (
+                <div className="flex justify-center items-center w-full h-full">
+                  <div className="animate-spin rounded-full h-5 w-5 border-t-2 border-b-2 border-[#6200EA]"></div>
+                </div>
+              ) 
               :
-              <span className=' text-[red] italic'>#not found#</span>
-              }
+              (
+                <span className=" text-[red] italic"># not found #</span>
+              )
+              : 
+              wordExist 
+              ? 
+              (
+                transcriptions && transcriptions.length > 0 ? (
+                  <span># {transcriptions[0]} #</span>
+                ) 
+                :
+                 (
+                  "/##/"
+                )
+              ) 
+              : 
+              (
+                <span className=" text-[red] italic"># not found #</span>
+              )}
             </span>
           </div>
-          <div className='flex flex-col h-max w-full m-4 gap-5 overflow-y-auto'>
-            {wordExist ? 
+          <div
+            className={`flex flex-col h-max w-full gap-5 ${
+              isLoading ? "overflow-hidden" : "overflow-y-auto"
+            }`}
+          >
+            {
+            isLoading 
+            ? 
+            wordExist 
+            ?
+            (
+              <Player
+                src="https://lottie.host/9f110dcf-ff4f-4cd9-8c1b-e6f3b8a78983/GzKZe7Tgo3.json"
+                background="transparent"
+                speed={1}
+                loop
+                autoplay
+                className="w-[200px] h-[200px] mx-auto"
+              />
+            )
+            :
+            (
+              <span className="text-[red] flex justify-center items-center font-bold capitalize">
+                not found
+              </span>
+            )
+             : 
+             wordExist 
+             ? 
+             (
+              Object.entries(groupedDefinitions).map(([partOfSpeech, defs]) => (
+                <div key={partOfSpeech}>
+                  <h3 className="font-semibold mt-4">
+                    {partOfSpeech.charAt(0).toUpperCase() +
+                      partOfSpeech.slice(1)}
+                  </h3>
+                  {defs.map((definition, index) => (
+                    <div key={index}>
+                      <p>
+                        {defs.length > 1 ? (
+                          <span className="text-[#6200EA] text-[20px]">•</span>
+                        ) : (
+                          ""
+                        )}
+                        {definition}
+                      </p>
+                      {index < defs.length - 1 && <hr />}
+                    </div>
+                  ))}
+                </div>
+              ))
+            ) : (
+              <span className="text-[red] flex justify-center items-center font-bold capitalize">
+                not found
+              </span>
+            )}
+            {/* {wordExist ? 
             Object.entries(groupedDefinitions).map(([partOfSpeech, defs]) => (
               <div key={partOfSpeech}>
                 <h3 className="font-semibold mt-4">
@@ -169,35 +271,66 @@ export const SearchWord = () => {
             ))
             : 
             <span className='text-[red] flex justify-center items-center font-bold capitalize'>not found</span>
-          }
-            <span className='italic text-[15px] text-[#6200EA] flex justify-end items-center w-full h-max'> 
-              <span className='font-bold '>• </span> latin origin <span className='font-bold'> •</span>
+          } */}
+            <span className="italic text-[15px] text-[#6200EA] flex justify-end items-center w-full h-max">
+              <span className="font-bold ">• </span> latin origin{" "}
+              <span className="font-bold"> •</span>
             </span>
           </div>
         </div>
         <div className="flex-1 h-[300px] flex flex-col p-4">
           <div className="flex justify-between w-full">
-            <h1 className='text-[18px] font-bold'>Use Case</h1>
-            <span className='text-[16px] border-2 border-[#6200EA] py-1 px-2 rounded-full'>Verb </span>
+            <h1 className="text-[18px] font-bold">Use Case</h1>
+            <span className="text-[16px] border-2 border-[#6200EA] py-1 px-2 rounded-full">
+              Verb{" "}
+            </span>
           </div>
           <div className="flex flex-col w-full h-max mt-4 overflow-y-auto">
-            {wordExist ?
-            Object.entries(groupedExamples).map(([partOfSpeech, examples]) => (
-              <div key={partOfSpeech}>
-                <h3 className="font-bold text-lg">{partOfSpeech}</h3>
-                <ul className="pl-4">
-                  {examples.map((example, index) => (
-                    <li key={index}>
-                      <span className="font-bold">•</span> {example}
-                      <hr className="my-2 border-gray-300" />
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))
+          {
+            isLoading 
+            ? 
+            wordExist 
+            ?
+            (
+              <Player
+                src="https://lottie.host/9f110dcf-ff4f-4cd9-8c1b-e6f3b8a78983/GzKZe7Tgo3.json"
+                background="transparent"
+                speed={1}
+                loop
+                autoplay
+                className="w-[200px] h-[200px] mx-auto"
+              />
+            )
             :
-            <span className='text-[red] flex justify-center items-center font-bold capitalize'>Not Found</span>
-          }
+            (
+              <span className="text-[red] flex justify-center items-center font-bold capitalize">
+                not found
+              </span>
+            )
+             : 
+             wordExist 
+             ? 
+             (
+              Object.entries(groupedExamples).map(
+                ([partOfSpeech, examples]) => (
+                  <div key={partOfSpeech}>
+                    <h3 className="font-bold text-lg">{partOfSpeech}</h3>
+                    <ul className="pl-4">
+                      {examples.map((example, index) => (
+                        <li key={index}>
+                          <span className="font-bold">•</span> {example}
+                          <hr className="my-2 border-gray-300" />
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )
+              )
+            ) : (
+              <span className="text-[red] flex justify-center items-center font-bold capitalize">
+                not found
+              </span>
+            )}
           </div>
         </div>
       </div>
