3acb4b411f5585e3f772e5aa40c881665d28eaaf
kennedyifeadi <kennedyifeadi@gmail.com>
2024-11-04 02:31:59 +0100

    Refactor Dailyword component to fetch and display a random word with its meaning and transcription

--- src/components/Dailyword.jsx
+++ src/components/Dailyword.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 export const Dailyword = () =>{
     const [randomWord, setRandomWord] = useState("");
     const [wordMeaning, setWordMeaning] = useState("");
+    const [wordTranscription, setWordTranscrpition] = useState('');
 
         useEffect(() => {
             const runRandomWord = async ()=>{
@@ -16,7 +17,7 @@ export const Dailyword = () =>{
                 }
 
 
-                if(!lastRunDate || (now - lastRunDate) >=  60000){
+                if(!lastRunDate || (now - lastRunDate) >=  24*60*60*1000){
                         try {
                             const response = await axios.get("https://random-word-api.herokuapp.com/word");
                             const word = response.data[0];
@@ -37,7 +38,12 @@ export const Dailyword = () =>{
                 try {
                     const res = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${randomWord}`);
                     const definition = res.data[0].meanings[0].definitions[0].definition;
+                    const transcription = res.data.flatMap((e) =>
+                        e.phonetics.map((p) => p.text)
+                      )
                     console.log(definition)
+                    console.log(transcription)
+                    setWordTranscrpition(transcription)
                     setWordMeaning(definition);
                 } catch (error) {
                     console.log(error)
@@ -52,10 +58,10 @@ export const Dailyword = () =>{
             <h1 className="font-bold text-[24px]"> <span className="text-[#6200EA]">Word</span> of the <span className="text-[#FF5722]">day</span>!</h1>
             <div className="flex gap-5">
                 <h1 className="font-bold text-[18px] capitalize">{randomWord}</h1>
-                <span className="text-[16px] border-2 border-[#6200EA] px-2 rounded-full"># <i>/eailu/</i> #</span>
+                <span className="text-[16px] border-2 border-[#6200EA] px-2 rounded-full"># <i>{wordTranscription}</i> #</span>
             </div>
             <div className="felx justify-center items-center w-max">
-                <p className="max-w-[520px] w-max italic text-wrap break-all ">{wordMeaning}</p>
+                <p className="max-w-[620px] w-max italic text-wrap break-all ">{wordMeaning}</p>
             </div>
         </div>
     )
