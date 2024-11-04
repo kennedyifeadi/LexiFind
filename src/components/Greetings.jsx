604819933a7144c946857f744a2385761701e960
kennedyifeadi <kennedyifeadi@gmail.com>
2024-11-04 16:48:41 +0100

    optimized responsiveness for the Greetings component

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -1,30 +1,31 @@
 import { Player } from '@lottiefiles/react-lottie-player';
 
-export const Greetings = () =>{
+export const Greetings = () => {
     let date = new Date();
     let time = date.getHours();
     let greeting = "";
-    if(time < 12){
+    if (time < 12) {
         greeting = "Morning";
-    }else if(time < 18){
+    } else if (time < 18) {
         greeting = "Afternoon";
-    }else{
+    } else {
         greeting = "Evening";
     }
 
-    return(
-        <div className="w-full h-[25%] px-6 py-4 z-20">
-            <h1 className="font-bold text-[48px] relative w-[30%]  "><span className="text-[#6200EA]">Hey</span> there 
-            <Player
-      src="https://lottie.host/17fdfd16-a108-4b1f-8a85-786af371d58b/Guf8MUcNat.json"
-      background="transparent"
-      speed={1}
-      loop
-      autoplay
-      className="w-20 h-20 rotate-[-50deg] absolute top-[-10px] right-2"
-    />
-    </h1>
-            <h1 className="font-bold text-[48px]">Good <span className="text-[#FF5722]">{greeting}!</span></h1>
+    return (
+        <div className="w-full h-max px-4 py-2 md:px-6 md:py-4 z-20">
+            <h1 className="font-bold text-[24px] md:text-[48px] relative w-full md:w-[30%] ">
+                <span className="text-[#6200EA]">Hey</span> there
+                <Player
+                    src="https://lottie.host/17fdfd16-a108-4b1f-8a85-786af371d58b/Guf8MUcNat.json"
+                    background="transparent"
+                    speed={1}
+                    loop
+                    autoplay
+                    className="md:w-20 md:h-20 w-12 h-12 md:left-[15.2rem] left-[7.2rem]  rotate-[-50deg] absolute top-[-10px] "
+                />
+            </h1>
+            <h1 className="font-bold text-[36px] md:text-[48px]">Good <span className="text-[#FF5722]">{greeting}!</span></h1>
         </div>
     )
 }
