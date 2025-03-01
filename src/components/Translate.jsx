8b2c8a4a66db62a9e466ae92b43c31f34db92cbb
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-01 08:27:02 +0100

    Refactor UI layout in Translate.jsx and optimize color handling

--- src/components/Translate.jsx
+++ src/components/Translate.jsx
@@ -1,11 +1,11 @@
 
 export const TranslateWriteUp = () => {
   return (
-    <div className="w-full flex flex-col px-7">
-      <h1 className="w-[40%] font-bold text-[48px] flex flex-col">
+    <div className="w-full flex flex-col px-3 md:px-7">
+      <h1 className="w-full font-bold text-3xl md:text-5xl flex flex-wrap md:flex-col">
         Generative <span>speech to text</span> <span>translation</span>
       </h1>
-      <p className="w-[50%] text-[gray] text-[15px]">Automated AI dubbing when quality matters</p>
+      <p className="md:w-[50%] text-[gray] text-[15px]">Automated AI dubbing when quality matters</p>
     </div>
   )
 }
