d4a82fdcbe30e141c8258e05e6ae43bc246e305b
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-04 01:08:37 +0100

    Refactor App component to add TranslateProvider and DashboardProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { BrowserRouter } from "react-router-dom"
 import { Nav } from "./components/Nav"
 import { AnimatedRoutes } from "./components/AnimatedRoutes";
+import { TranslateProvider } from "./context/TranslateContext";
+import { DashboardProvider } from "./context/DashboardContext";
 
 
 function App() {
 
   return (
     <BrowserRouter>
-      <div className="md:w-full h-[160dvh] md:h-[100dvh] relative flex bg-[#f0eeee73] md:overflow-hidden">
-      <Nav />
-      <AnimatedRoutes />
-      </div>
+      <TranslateProvider>
+        <DashboardProvider>
+        <div className="md:w-full h-[160dvh] md:h-[100dvh] relative flex bg-[#f0eeee73] md:overflow-hidden">
+        <Nav />
+        <AnimatedRoutes />
+        </div>
+        </DashboardProvider>
+      </TranslateProvider>
     </ BrowserRouter>
 
   )
