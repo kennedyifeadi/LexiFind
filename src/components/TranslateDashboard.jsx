e1d5468104dc82c91331dd0a3dbd4c6be9f5682f
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-05 19:11:46 +0100

    Refactor TranslateDashboard component to update context imports and improve UI layout

--- src/components/TranslateDashboard.jsx
+++ src/components/TranslateDashboard.jsx
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 import { useContext} from 'react';
 import { TranslateContext } from '../context/TranslateContext'
-import { DashboardContext } from '../context/DashboardContext';
+import { TranslatorDashboardContext } from '../context/TranslatorDashboardContext';
 
 export const TranslateDashboard = ({ids, text, icon, title, color }) => {
   const {isClicked, setIsClicked} = useContext(TranslateContext);
-  const {setId} = useContext(DashboardContext);
+  const {setId} = useContext(TranslatorDashboardContext);
   const handleClick = () => {
     setIsClicked(!isClicked);
     setId(ids)
@@ -21,7 +21,7 @@ export const TranslateDashboard = ({ids, text, icon, title, color }) => {
       : ""; 
 
   return (
-    <div className='flex relative w-full md:w-full h-max'>
+    <div className='flex relative w-full md:w-[30%] h-max'>
     <div className="flex w-[100%] flex-col justify-between duration-500 ease-in-out cursor-pointer active:scale-75 p-4 shadow-md bg-white rounded-md h-[300px] md:h-[200px]" onClick={handleClick}>
       <div className={`${colorClass == '#daf4fe' ? "bg-[#daf4fe] text-[#32c5ff]" : colorClass == '#f8e3f8' ? 'bg-[#ffc7ff] text-[#fd2dfd]' : colorClass == '#f8e5ce' ? 'bg-[#fcdcb5] text-[#ffa230]' : ''}  w-12 h-12 rounded-full flex justify-center items-center text-[22px] font-semibold`}>{icon}</div>
       <div className="flex flex-col">
