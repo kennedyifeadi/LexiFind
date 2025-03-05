51e6e734ba4d627709afb44ecb5d51421a380b98
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-05 19:11:08 +0100

    Refactor AudioToText component to improve UI layout and add text input field

--- src/components/translatorCards/AudioToText.jsx
+++ src/components/translatorCards/AudioToText.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export const AudioToText = () => {
   return (
-    <div>AudioToText</div>
+    <div className='w-full h-full flex p-4'>
+      <div className='w-full h-full flex flex-col'>
+        <div className='w-full h-[70%] border-dashed border-[gray] border rounded-xl flex flex-col'>
+          <div className='w-full h-[50%] flex border-b border-dashed'>
+            <textarea name="" id="" className='w-full p-4 h-full bg-transparent outline-none'></textarea>
+          </div>
+          <div className='w-full h-[50%] flex'></div>
+        </div>
+        <div></div>
+      </div>
+    </div>
   )
 }
