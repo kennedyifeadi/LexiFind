dc70251392da07d4cf2cb03f5d66e431e58d9aa9
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-09 22:51:19 +0100

    Refactor App component to add ThesaurusProvider for context management

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { AnimatedRoutes } from "./components/AnimatedRoutes";
 import { TranslatorDashboardProvider } from "./context/TranslatorDashboardContext";
 import { ThesaurusDashboardProvider } from "./context/ThesaurusDashboardContext";
 import { TranslateProvider } from "./context/TranslateContext";
+import { ThesaurusProvider } from "./context/thesaurusContext";
 
 
 function App() {
@@ -13,10 +14,12 @@ function App() {
       <TranslateProvider>
         <TranslatorDashboardProvider>
           <ThesaurusDashboardProvider>
-            <div className="md:w-full h-[160dvh] md:h-[100dvh] relative flex bg-[#f0eeee73] md:overflow-hidden">
-            <Nav />
-            <AnimatedRoutes />
-            </div>
+            <ThesaurusProvider>
+              <div className="md:w-full h-[160dvh] md:h-[100dvh] relative flex bg-[#f0eeee73] md:overflow-hidden">
+              <Nav />
+              <AnimatedRoutes />
+              </div>
+            </ThesaurusProvider>
          </ThesaurusDashboardProvider>
         </TranslatorDashboardProvider>
       </TranslateProvider>
