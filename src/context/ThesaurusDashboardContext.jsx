e6ff1df6dfd7f23488e1b9c6f0a6cdf2d50d7c9b
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-09 22:51:40 +0100

    Refactor ThesaurusDashboardContext to add thesaurusTitle state

--- src/context/ThesaurusDashboardContext.jsx
+++ src/context/ThesaurusDashboardContext.jsx
@@ -2,9 +2,11 @@ import React, { createContext, useState } from 'react'
 export const ThesaurusDashboardContext = createContext()
 export const ThesaurusDashboardProvider = ({children}) => {
     const [id, setId] = useState(null);
+    const [thesaurusTitle, setThesaurusTitle] = useState("")
+
   return (
     <div>
-        <ThesaurusDashboardContext.Provider value={{id, setId}}>
+        <ThesaurusDashboardContext.Provider value={{id, setId, thesaurusTitle, setThesaurusTitle}}>
           {children}
         </ThesaurusDashboardContext.Provider>
     </div>
