a55447030d820d83e45d76f9f5656d9c03bf9285
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-11 18:20:37 +0100

    Refactor App component to remove unused ThesaurusProvider

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { AnimatedRoutes } from "./components/AnimatedRoutes";
 import { TranslatorDashboardProvider } from "./context/TranslatorDashboardContext";
 import { ThesaurusDashboardProvider } from "./context/ThesaurusDashboardContext";
 import { TranslateProvider } from "./context/TranslateContext";
-import { ThesaurusProvider } from "./context/thesaurusContext";
 
 
 function App() {
@@ -14,12 +13,10 @@ function App() {
       <TranslateProvider>
         <TranslatorDashboardProvider>
           <ThesaurusDashboardProvider>
-            <ThesaurusProvider>
               <div className="md:w-full h-[160dvh] md:h-[100dvh] relative flex bg-[#f0eeee73] md:overflow-hidden">
               <Nav />
               <AnimatedRoutes />
               </div>
-            </ThesaurusProvider>
          </ThesaurusDashboardProvider>
         </TranslatorDashboardProvider>
       </TranslateProvider>
