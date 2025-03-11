f7f3c66c3c311ba370b9700bd5400b730ada8b97
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-11 18:21:18 +0100

    Refactor ThesaurusDashboardContext component to remove unused thesaurusTitle state

--- src/context/ThesaurusDashboardContext.jsx
+++ src/context/ThesaurusDashboardContext.jsx
@@ -2,11 +2,10 @@ import React, { createContext, useState } from 'react'
 export const ThesaurusDashboardContext = createContext()
 export const ThesaurusDashboardProvider = ({children}) => {
     const [id, setId] = useState(null);
-    const [thesaurusTitle, setThesaurusTitle] = useState("")
 
   return (
     <div>
-        <ThesaurusDashboardContext.Provider value={{id, setId, thesaurusTitle, setThesaurusTitle}}>
+        <ThesaurusDashboardContext.Provider value={{id, setId}}>
           {children}
         </ThesaurusDashboardContext.Provider>
     </div>
